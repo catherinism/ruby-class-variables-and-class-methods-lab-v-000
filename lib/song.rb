588ad8eb4ff1629279f6e2d791f0a78fa6cb1cde
Catherine Salazar <catcreates13@gmail.com>
2018-03-08 03:53:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,11 +32,14 @@ def self.genres
 end
 
 def self.genre_count
-  genre_count = {}
-  @@genres.each do |genre|
-    puts "#{genre}: #{count}"
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
-
-end
-
 end
